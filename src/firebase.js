7ce1454698f892a9d97960067d396663adf3f911
unknown <fachrezaghifary@gmail.com>
2020-09-25 13:48:52 +0700

    add firebase

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAjYw3y8BvlqYYqNBKxQSWb8y1QKJrhYsM",
+    authDomain: "gras-messanger.firebaseapp.com",
+    databaseURL: "https://gras-messanger.firebaseio.com",
+    projectId: "gras-messanger",
+    storageBucket: "gras-messanger.appspot.com",
+    messagingSenderId: "612071402408",
+    appId: "1:612071402408:web:513ce978ac09ee244d4b9d",
+    measurementId: "G-GRVBZRBB9Q"
+  };
